d371adac606708fedcea5ab5247d96b80071fcaf
muhammad.sabih <sabih544>
2023-03-13 20:03:50 +0500

    added preprocessing class

--- model.py
+++ model.py
@@ -21,3 +21,33 @@ class OnnxModel():
         ort_inputs = {self.ort_session.get_inputs()[0].name: PreProcessing.to_numpy(model_input)}
         ort_outs = self.ort_session.run(None, ort_inputs)
         return np.argmax(ort_outs[0])
+
+
+class PreProcessing():
+
+    def preprocess_image(self, img, img_size):
+        resize = self.resize_img(img_size)
+        crop = self.center_crop_img(img_size)
+        to_tensor = self.convert_img_to_tensor()
+        normalize = self.normalize_img()
+        resized_image = resize(img)
+        cropped_image = crop(resized_image)
+        img_tensor = to_tensor(cropped_image)
+        normalized_img = normalize(img_tensor)
+        return normalized_img
+
+    def resize_img(self, img_size):
+        return transforms.Resize(img_size)
+
+    def center_crop_img(self, img_size):
+        return transforms.CenterCrop(img_size)
+
+    def convert_img_to_tensor(self):
+        return transforms.ToTensor()
+
+    def normalize_img(self):
+        return transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+
+    @staticmethod
+    def to_numpy(tensor):
+        return tensor.detach().cpu().numpy() if tensor.requires_grad else tensor.cpu().numpy()
